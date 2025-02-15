019506057da7039f9b26976dccca2cc02b9336e8
ANISH CHAUHAN <146816421+ANISH-CHAUHAN105@users.noreply.github.com>
2025-02-15 10:23:51 +0530

    Update page.js

--- app/sign-up/[[...sign-up]]/page.js
+++ app/sign-up/[[...sign-up]]/page.js
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50 dark:bg-gray-900 transition-all duration-300">
-      <SignUp />
+      <SignUp fallbackRedirectUrl="/dashboard" />
     </div>
   );
 }
