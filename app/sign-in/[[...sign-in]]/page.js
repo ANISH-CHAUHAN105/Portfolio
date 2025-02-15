de5f2a47f7040d812231d8581373e2e4935dfd33
ANISH CHAUHAN <146816421+ANISH-CHAUHAN105@users.noreply.github.com>
2025-02-15 10:23:30 +0530

    Update page.js

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50 dark:bg-gray-900 transition-all duration-300">
-      <SignIn />
+      <SignIn fallbackRedirectUrl="/dashboard" />
     </div>
   );
 }
